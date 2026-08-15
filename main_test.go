4e419abd73b7bb08a0b1ac6768ea55c0f0fc9791
Артем Васенин <artem.skobar@yandex.ru>
2026-08-15 10:24:16 +0300

    added tests

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"strings"
 	"testing"
 
@@ -46,5 +47,78 @@ func TestCafeWhenOk(t *testing.T) {
 		handler.ServeHTTP(response, req)
 
 		assert.Equal(t, http.StatusOK, response.Code)
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	city := "moscow"
+	cafeLen := len(cafeList[city])
+
+	requests := []struct {
+		count int
+		want  int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{100, cafeLen},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/cafe?city="+city+"&count="+strconv.Itoa(v.count), nil)
+		handler.ServeHTTP(response, req)
+
+		result := strings.TrimSpace(response.Body.String())
+		var count int
+		if result != "" {
+			count = len(strings.Split(result, ","))
+		}
+
+		assert.Equal(t, v.want, count)
+	}
+}
+
+func TestCafeSearch(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+	city := "moscow"
+	cafeLen := len(cafeList[city])
+
+	requests := []struct {
+		search    string
+		wantCount int
+	}{
+		{"фасоль", 0},
+		{"кофе", 2},
+		{"вилка", 1},
+		{"", cafeLen},
+	}
+
+	for _, v := range requests {
+		response := httptest.NewRecorder()
+		req := httptest.NewRequest("GET", "/cafe?city="+city+"&search="+v.search, nil)
+		handler.ServeHTTP(response, req)
+
+		result := strings.TrimSpace(response.Body.String())
+		var count int
+		contains := true
+
+		if result != "" {
+			list := strings.Split(result, ",")
+			count = len(list)
+
+			for _, str := range list {
+				upperStr := strings.ToUpper(str)
+				upperSearch := strings.ToUpper(v.search)
+				if !strings.Contains(upperStr, upperSearch) {
+					contains = false
+					break
+				}
+			}
+		}
+
+		assert.Equal(t, v.wantCount, count)
+		assert.Equal(t, true, contains)
 	}
 }
